1c6adb56cb2988fd6542e86622c0225b5cda3797
MatheusFCabral <107444971+MatheusFCabral@users.noreply.github.com>
2022-06-13 21:18:31 -0300

    big bang

---
+++ studio-integral/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './main/home/home.component';
+import { MainComponent } from './main/main.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'start/login', pathMatch: 'full'},
+
+  {
+    path: 'main', component: MainComponent, children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full'},
+      { path: 'home', component: HomeComponent, pathMatch: 'full' },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
